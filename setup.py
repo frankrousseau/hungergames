dd157e5c8cf510d6ccb91907369ee25eb1b2f399
Frank Rousseau <frank.rousseau@free.fr>
2016-09-08 22:06:26 +0200

    Add setup file (packaging)

---
+++ setup.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass into py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    'hungergames',
+]
+
+requires = []
+test_requirements = []
+
+with open('hungergames/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='hungergames',
+    version=version,
+    description='Easy querying of OpenFoodFacts.',
+    long_description=readme,
+    author='Pierre Slamich',
+    author_email='pierre@openfoodfacts.org',
+    url='http://openfoodfacts.org',
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    package_dir={'hungergame': 'hungergame'},
+    include_package_data=True,
+    install_requires=requires,
+    license='Apache 2.0',
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ),
+    cmdclass={'test': PyTest},
+    tests_require=test_requirements,
+    extras_require={
+    },
+)
