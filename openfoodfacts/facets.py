640375daa8b6e65c4fd7fabe71a41789bcd522db
Frank Rousseau <frank.rousseau@free.fr>
2016-09-09 18:44:34 +0200

    Generate facet fetching function automatically.

--- openfoodfacts/facets.py
+++ openfoodfacts/facets.py
@@ -1,16 +1,17 @@
-import requests
-
-API_URL = "http://world.openfoodfacts.org/"
-
-def fetch(path):
-    response = requests.get("%s/%s.json" % (API_URL, path))
-    return response.json()
-
-def get_traces():
-    return fetch('traces')['tags']
-
-def get_allergens():
-    return fetch('allergens')['tags']
-
-def get_product(barcode):
-    return fetch('api/v0/product/%s' % barcode)
+facets = [
+    'traces',
+    'ingredients',
+    'stores',
+    'packaging_codes',
+    'packagings',
+    'brands',
+    'labels',
+    'categories',
+    'allergens',
+    'additives',
+    'countries',
+    'purchase_places',
+    'states',
+    'entry_dates',
+    'languages',
+]
