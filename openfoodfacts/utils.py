ad6b9aa94aeca4c9d0d33f230cc9d4189192a2e2
Frank Rousseau <frank.rousseau@free.fr>
2016-09-13 13:28:44 +0200

    Remove useless slash when fetching data

--- openfoodfacts/utils.py
+++ openfoodfacts/utils.py
@@ -8,5 +8,6 @@ Fetch data at a given path assuming that target match a json file and is
 located on the OFF API.
 """
 def fetch(path):
-    response = requests.get("%s/%s.json" % (API_URL, path))
+    print "%s%s.json" % (API_URL, path)
+    response = requests.get("%s%s.json" % (API_URL, path))
     return response.json()
